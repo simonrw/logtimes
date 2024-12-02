4af80efd0195549bc22b79a536a86565656eae81
Simon Walker <s.r.walker101@googlemail.com>
2024-12-02 23:42:32 +0000

    Set up initial project

---
+++ logtimes/__init__.py
@@ -0,0 +1,24 @@
+import sys
+from dateutil.parser import parse as parse_date
+
+def main():
+    start = None
+    for line in sys.stdin:
+        line = line.strip()
+
+        try:
+            d = line.split()[0]
+        except IndexError:
+            continue
+
+        dt = parse_date(d)
+        if not dt:
+            print(f"-1 {line}")
+
+        if start is None:
+            start = dt
+
+        delta = dt - start
+        print(f"{delta} {line}")
+
+
